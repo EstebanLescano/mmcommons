2e9ab1c746b5a62eacc7d60ef5d1711ec862a15b
EstebanLescano <estebanles22@gmail.com>
2025-08-08 07:51:38 -0300

    Initial commit

---
+++ src/test/java/org/lea/mmcommons/MmcommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lea.mmcommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmcommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
