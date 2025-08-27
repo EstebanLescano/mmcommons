dc79b12a99e0439bd30c710cd16b1181c6e32722
EstebanLescano <estebanles22@gmail.com>
2025-08-27 13:46:55 -0300

    change en filter

--- src/main/java/org/lea/mmcommons/configurations/SecurityConfig.java
+++ src/main/java/org/lea/mmcommons/configurations/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.lea.mmcommons.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -16,13 +17,13 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
+                        .requestMatchers("/public/**").permitAll() // rutas públicas
+                        .anyRequest().authenticated()             // el resto requiere token
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt
-                                .jwtAuthenticationConverter(jwtAuthConverter())
-                        )
+                        .jwt(Customizer.withDefaults())
                 );
+
         return http.build();
     }
 
